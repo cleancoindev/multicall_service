dec4da45a535c7a6e62add698112cb6c05381051
Fernandez Gonzalo <fernandezgonzalosebastia@gmail.com>
2019-10-02 12:26:07 -0300

    run with all networks

--- server.js
+++ server.js
@@ -3,29 +3,38 @@
 const express = require('express');
 const multicall = require('@makerdao/multicall')
 const bodyParser = require('body-parser');
-const {getConfig} = require('./multicall_config');
+const {configs} = require('./multicall_config');
 
 // express vars
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
-// multicall vars
-const NETWORK = process.env.NETWORK || "ropsten"
-const multicallConfig = getConfig(NETWORK);
 
 // App
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/multicall', async (req, res, next) => {
-  console.log("Requested: ", req.body);
+async function req_multicall(query, network_config) {
+  const response = await multicall.aggregate(query, network_config);
+  return response.results;
+};
+
+async function handle_request(req, res, next) {
+  var network = req.path.replace(/\//g, "")
   try {
-    const response = await multicall.aggregate(req.body, multicallConfig)
-    console.log("Response: ", response.results);
-    res.json(response.results);
+    const response = await req_multicall(req.body, configs[network])
+    res.json(response);
   } catch (e) {
     next(e)
   }
-});
+};
+
+app.get('/mainnet', handle_request);
+
+app.get('/ropsten', handle_request);
+
+app.get('/kovan', handle_request);
+
+app.get('/rinkeby', handle_request);
 
 app.listen(PORT, HOST);
-console.log(`Running ${NETWORK} on http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}`)
